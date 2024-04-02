2c059dc7eb3a86dac853821d0f6def5eaf977089
fartpluswetone <jba236236@gmail.com>
2024-04-02 13:52:23 -0500

    testing navbar on my pages

--- src/routes/ben/+page.svelte
+++ src/routes/ben/+page.svelte
@@ -2,8 +2,20 @@
     import { base } from "$app/paths";
     
 </script>
+<!-- main text -->
 <h1>hi</h1>
 <p> my name is ben :) </p>
-<a href="{base}/rudy/">
-Rudy's Page
-</a>
+<!-- testing navbar stuff -->
+<nav>
+    <ul>
+    <li>
+    <a href="{base}/rudy/"> Rudy's page </a>
+    </li>
+    <li>
+    <a href="{base}/colton/">Colton's page </a>
+    </li>
+    <li>
+    <a href="{base}/about/"> About us </a>
+    </li>
+    </ul>    
+</nav>
