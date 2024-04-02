fdac7a95417bdfac9632f63b6760d8ebe2021c4f
Colton <TheUrgeTM>
2024-04-02 10:54:56 -0500

    Add Page to link to rudy

--- src/routes/colton/+page.svelte
+++ src/routes/colton/+page.svelte
@@ -1,7 +1,10 @@
+
 <script>
+  import { base } from "$app/paths";
 
 </script>
 
 <h1>Hello, World</h1>
 <h2>Im a real hacker boy now</h2>
 <p>Kill Yourself</p>
+<a href="{base}/rudy"> Go to Rudys' Page</a>
