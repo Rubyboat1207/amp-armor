e58e1233101b92354fce9d693a905213c927967a
Rubyboat1207 <rudy@rudysoliz.com>
2024-04-09 15:34:42 -0500

    main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,9 @@
     <div class="mr-auto place-self-center lg:col-span-7">
       <HeroHeader h1Class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white" pClass="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
         <svelte:fragment slot="h1">Keeping you and your car safe</svelte:fragment>
-        <svelte:fragment slot="paragraph">With the green future promised by EV cars, saftey is our Number One Concern. Our EV Car Battery extiguisher/early warning system/whatevr we want it to be helps you when you may need it the most.</svelte:fragment>
-        <a href="/"><Button size="xl" color="red" class="inline-flex items-center justify-center mr-3">Get started<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
-        <a href="/"><Button color="light" size="xl" class="inline-flex items-center justify-center">Speak to Sales</Button> </a>
+        <svelte:fragment slot="paragraph">With the green future promised by EV cars, saftey is our <strong>number one concern</strong>. Our EV Car Battery extiguisher/early warning system/whatevr we want it to be helps you when you may need it the most.</svelte:fragment>
+        <a href="/"><Button size="xl" color="red" class="inline-flex items-center justify-center mr-3">Learn More<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
+        <a href="/"><Button color="light" size="xl" class="inline-flex items-center justify-center">Our Solution</Button> </a>
       </HeroHeader>
     </div>
     <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
