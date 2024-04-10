d64f7995e50ed8bd7ef7a58017d333f9b0967c4e
Rubyboat1207 <rudy@rudysoliz.com>
2024-04-10 12:05:25 -0500

    style (:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
   import { ArrowRightOutline } from 'flowbite-svelte-icons';
   import render from "../assets/product_render.png"
   import highway_fire from "../assets/highway_fire.png"
+  import house_explode from "../assets/house_explode.webp"
 
 
   export const images = [
@@ -14,14 +15,14 @@
       title: 'Highway Fire'
     },
     {
-      alt: 'Cristina Gottardi',
-      src: '/images/carousel/cristina-gottardi-CSpjU6hYo_0-unsplash.webp',
-      title: 'cristina-gottardi-CSpjU6hYo_0-unsplash.com'
+      alt: 'Exploded house',
+      src: house_explode,
+      title: 'house exploded'
     },
     {
-      alt: 'Johannes Plenio',
+      alt: 'Leaf in Ukraine',
       src: '/images/carousel/johannes-plenio-RwHv7LgeC7s-unsplash.webp',
-      title: 'johannes-plenio-RwHv7LgeC7s-unsplash.com'
+      title: 'Ukraine Leaf'
     }
   ];
 </script>
@@ -32,7 +33,7 @@
       <HeroHeader h1Class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white" pClass="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
         <svelte:fragment slot="h1">Keeping you and your car safe</svelte:fragment>
         <svelte:fragment slot="paragraph">With the green future promised by EV cars, saftey is our <strong>number one concern</strong>. Our EV Car Battery extiguisher/early warning system/whatevr we want it to be helps you when you may need it the most.</svelte:fragment>
-        <a href="/"><Button size="xl" color="red" class="inline-flex items-center justify-center mr-3">Learn More<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
+        <a href="/"><Button size="xl" color="primary" class="inline-flex items-center justify-center mr-3">Learn More<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
         <a href="/"><Button color="light" size="xl" class="inline-flex items-center justify-center">Our Solution</Button> </a>
       </HeroHeader>
     </div>
@@ -42,6 +43,12 @@
   </div>
 </Section>
 
-<div>
+<div class="flex pb-10">
+  <div class="w-1/2">
+    <h1 class="font-black text-5xl">The present danger of EVs</h1>
+    <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 p-4">Whether on the road, plugged in or simply unattended EVs can pose a serious danger to those around them.</p>
+  </div>
+  <div class="w-1/2">
   <Carousel {images} duration="5000"/>
+  </div>
 </div>
