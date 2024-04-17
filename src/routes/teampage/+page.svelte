5dbb3eef2b1c63a9c3d460fbc4f02f150414712b
Colton <TheUrgeTM>
2024-04-17 14:00:05 -0500

    Fixed the solution button link

--- src/routes/teampage/+page.svelte
+++ src/routes/teampage/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+    import { Section, News, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
     import { base } from "$app/paths";
-
+    import { Carousel } from 'flowbite-svelte';
+    import { Button } from 'flowbite-svelte';
+    import { ArrowRightOutline } from 'flowbite-svelte-icons'
+    ;
 </script>
 
