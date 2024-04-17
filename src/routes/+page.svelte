a2ccc43095a3f92f12868c20f1f06e3d56af0d0e
Colton <TheUrgeTM>
2024-04-17 15:42:33 -0500

    change of text and some formatting for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,9 +32,9 @@
 <Section name="heroVisual">
   <div class="flex mt-10">
     <div class="mr-auto place-self-center lg:col-span-7">
-      <HeroHeader h1Class="max-w-2xl mb-4 text-4xl text-primary-500 font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white" pClass="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-        <svelte:fragment slot="h1">Keeping you and your car safe</svelte:fragment>
-        <svelte:fragment slot="paragraph">With the green future promised by EV cars, saftey is our <strong>number one concern</strong>. Our EV Car Battery extiguisher helps you when you may need it the most.</svelte:fragment>
+      <HeroHeader h1Class="max-w-2xl mb-4 text-4xl text-primary-500 font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl" pClass="max-w-2xl mb-6 font-light text-primary-400 lg:mb-8 md:text-lg lg:text-xl">
+        <svelte:fragment slot="h1">Keeping You and Your Car Safe</svelte:fragment>
+        <svelte:fragment slot="paragraph">Electric, hybrids, and other plug-in vehicles are on the rise in almost every state and country as our modern world begins the transition away from fossil fuels and towards green energy. With the ever increasing popularity of Electric Vehicles, the more risk there is for these vehicles to harm people, and property. <strong>Safety is our number one concern</strong>. Our EV Car Battery extiguisher/early warning system/whatevr we want it to be helps you when you may need it the most.</svelte:fragment>
         <NavButton page='oursolution' text='Learn More'/>
       </HeroHeader>
     </div>
@@ -46,13 +46,16 @@
 
 <div class="flex justify-center">
   <div class="flex pb-10 max-w-screen-xl lg:w-11/12 flex-col lg:flex-row">
+    <div class="lg:w-1/2 lg:block hidden lg:mr-5">
+      <Carousel class="border-2 border-electric-cyan-50" style="box-shadow: 0px 0px 015px #00ffff;" {images} duration="5000"/>
+    </div>
     <div class="w-full lg:w-1/2 lg:mr-5">
       <h1 class="font-black text-5xl text-primary-500 lg:text-left text-center mt-5 lg:mt-0">The Danger of EVs</h1>
-      <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 p-4 lg:text-left text-center">Whether on the road, plugged in or simply unattended EVs can pose a serious danger to those around them.</p>
+      <p class="max-w-2xl mb-6 font-light text-primary-400 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 p-4 lg:text-left text-center">Whether on the road, plugged in or simply unattended EVs can pose a serious danger to those around them.</p>
       <NavButton page='precedence' text='Historical Precedence'/>
     </div>
-    <div class="lg:w-1/2">
-    <Carousel class="border-2 text-electric_cyan" style="box-shadow: 0px 0px 015px #00ffff;" {images} duration="5000"/>
+    <div class="lg:w-1/2 lg:hidden">
+    <Carousel class="border-2 border-electric-cyan-50 mt-5" style="box-shadow: 0px 0px 015px #00ffff;" {images} duration="5000"/>
     </div>
   </div>
 </div>
