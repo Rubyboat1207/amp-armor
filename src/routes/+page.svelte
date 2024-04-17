8deba0400b3700815673db617746380916881aef
Colton <TheUrgeTM>
2024-04-17 13:49:10 -0500

    fiddling with navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
   import highway_fire from "../assets/highway_fire.png"
   import house_explode from "../assets/house_explode.webp"
   import leaf from "../assets/leaf.webp"
-
+  import { base } from "$app/paths";
 
   export const images = [
     {
@@ -34,8 +34,8 @@
       <HeroHeader h1Class="max-w-2xl mb-4 text-4xl text-primary-500 font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white" pClass="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
         <svelte:fragment slot="h1">Keeping you and your car safe</svelte:fragment>
         <svelte:fragment slot="paragraph">With the green future promised by EV cars, saftey is our <strong>number one concern</strong>. Our EV Car Battery extiguisher/early warning system/whatevr we want it to be helps you when you may need it the most.</svelte:fragment>
-        <a href="/"><Button size="xl" class="inline-flex items-center justify-center mr-3 bg-primary-50">Learn More<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
-        <a href="/"><Button color="light" size="xl" class="inline-flex items-center justify-center">Our Solution</Button> </a>
+        <a href="/"><Button size="xl" class="inline-flex items-center justify-center mr-3 hover:bg-primary-50 bg-primary-200">Learn More<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button></a>
+        <a href="{base}/problemandsolution"><Button color="light" size="xl" class="inline-flex items-center justify-center">Our Solution</Button> </a>
       </HeroHeader>
     </div>
     <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
--- src/routes/problemandsolution/+page.svelte
+++ src/routes/problemandsolution/+page.svelte
@@ -1,3 +1,4 @@
+
 <script>
     import { base } from "$app/paths";
 
