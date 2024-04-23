868573db599a5b7198be388819e23f5f09141496
Colton <TheUrgeTM>
2024-04-23 12:27:23 -0500

    Changed Image in Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,14 @@
     import { base } from "$app/paths";
     import "../app.css";
     import background from '../assets/hexagon_tile.webp'
+    import logo from '../assets/AMPARMORtextlogo.webp'
 </script>
 <Navbar class=" border-b-2 border-primary-50 sticky top-0 z-50" style="background: url({background}); box-shadow: 0px 0px 15px #00ffff;">
     <NavBrand href="/">
-      <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-      <span class="self-center  whitespace-nowrap text-xl font-semibold text-primary-500 dark:text-white">Amp-Armor</span>
+      <img src="{logo}" class="me-10 h-10 sm:h-20" alt="Amp-Armor Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold text-primary-500 dark:text-white"></span>
     </NavBrand>
-    <NavHamburger  />
+    <NavHamburger/>
     <NavUl >
       <NavLi class="text-primary-400" href="{base}/teampages">Meet The Team</NavLi>
       <NavLi class="text-primary-400" href="{base}/about">About</NavLi>
