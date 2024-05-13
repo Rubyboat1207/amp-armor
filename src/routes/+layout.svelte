0a64ec4a29b9086c4f5b349ebf8d450b2e08a4f0
Colton <TheUrgeTM>
2024-05-13 11:01:12 -0500

    Fixed logo not sending back to home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,7 @@
   class=" border-b-2 border-primary-50 sticky top-0 z-50"
   style="background: url({background}); box-shadow: 0px 0px 15px #00ffff;"
 >
-  <NavBrand href="/{base}">
+  <NavBrand href="{base}/">
     <img src={logo} class="me-10 h-10 sm:h-20" alt="Amp-Armor Logo" />
     <span
       class="self-center whitespace-nowrap text-xl font-semibold text-primary-500 dark:text-white"
